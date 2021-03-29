6dc6e1a9b15a05c27fb0bbe1b13dea66c30cb833
Moses Giorgis <64659857+mgiorgis42@users.noreply.github.com>
2021-03-29 07:33:51 -0700

    Update toolbar.js

--- pages/components/toolbar.js
+++ pages/components/toolbar.js
@@ -10,7 +10,6 @@ export const Toolbar = () => {
                 <div onClick={() => router.push('/')}>Home</div>
                 <div onClick={() => router.push('/{slug}')}>Posts</div>
                 <div onClick={() => router.push('/about')}>About the Author</div>
-                <div onClick = {() => window.location.href ='http://mussiedevelops.com'}>Portfolio</div>
                 <div onClick = {() => window.location.href= 'https://linktr.ee/mussiedevelops'}>Connect w/ Me!</div>
             </div>
         </div>
